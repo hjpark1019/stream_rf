aef7585fe2ba5b5237c07f1dc615879be7ade0e1
박형진 <hjpark@HJPARK-MBPM3.local>
2025-11-10 09:27:06 +0900

    this is just test this is just test

---
+++ app.py
@@ -0,0 +1,18 @@
+# new test page
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+
+#페이지 기본 설정
+st.set_page_config(
+    page_icon="??",
+    page_title="테스트 배포",
+    layout="wide",
+)
+
+#페이지 헤더
+
+st.header("홈페이지에 오신것을 환영🤗")
+st.subheader("기능맛보기")
